96198699c1805a2ff0d598bbfed929dd3a79c73a
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-23 15:10:25 +0600

    Update Child2.java

--- src/Child2.java
+++ src/Child2.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Child2 {
+public class Child2 extends Parent implements Printable {
+    private String school;
+
+    public Child2(String name, int age, String school) {
+        super(name, age);
+        this.school = school;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Name: " + getName() + ", Age: " + getAge() + ", School: " + getSchool());
+    }
 }
