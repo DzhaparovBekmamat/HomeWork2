8ff4406e3ff5e04e59735a88a8d94645802ccf43
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-23 15:10:58 +0600

    Update Child3.java

--- src/Child3.java
+++ src/Child3.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Child3 {
+public class Child3 extends Parent implements Printable {
+    private String sport;
+
+    public Child3(String name, int age, String sport) {
+        super(name, age);
+        this.sport = sport;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Name: " + getName() + ", Age: " + getAge() + ", Sport: " + getSport());
+    }
 }
