806b107894b5c611ef4989275d4f848b2d206849
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-23 15:10:07 +0600

    Update Child1.java

--- src/Child1.java
+++ src/Child1.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Child1 {
+public class Child1 extends Parent implements Printable {
+    private String hobby;
+
+    public Child1(String name, int age, String hobby) {
+        super(name, age);
+        this.hobby = hobby;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Name: " + getName() + ", Age: " + getAge() + ", Hobby: " + getHobby());
+    }
 }
